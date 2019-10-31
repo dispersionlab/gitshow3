ca124225a563608fc6f4a3c6a7877d81952c0ee4
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2019-10-31 10:48:36 -0400

    the module can convert a continuous signal into a step sequencer

--- module3.js
+++ module3.js
@@ -11,30 +11,92 @@ function process(block) {
 	// Knob ranges from -5 to 5 octaves
 	// var probability = block.knobs[0] * 10 - 5
 
-	var probability = block.knobs[0]
+	// knob1 (k1) sets the probability of which output will be chosen
+	//var probability = block.knobs[0]
 
-	var switcher = Math.random() < probability
+	// switcher is a true/false value
+	//var switcher = Math.random() < probability
 
-	// Input follows 1V/oct standard
-	// pitch += block.inputs[0][0]		
 
 	
-  // display("LFO: " + freq + "Hz")
-	// Set all samples in output buffer
-	var deltaPhase = config.frameDivider * block.sampleTime
+		// Set all samples in output buffer
 	for (var i = 0; i < block.bufferSize; i++) {
-		// // Accumulate phase
-		// phase += deltaPhase
-		// // Wrap phase around range [0, 1]
-		// phase %= 1
-		// // Convert phase to sine output
+		
+		let switcher = Math.floor(block.inputs[0][i])
 
-		if (switcher === false) {
-			block.outputs[0][i] = block.inputs[0][i]
-			block.outputs[1][i] = 0
-		} else {
-			block.outputs[1][i] = block.inputs[0][i]
-			block.outputs[0][i] = 0
-		}	
+		display(switcher)
+		switch (switcher){
+			case 0:
+					block.outputs[0][i] = 10
+					block.outputs[1][i] = 0
+					block.outputs[2][i] = 0
+					block.outputs[3][i] = 0
+					block.outputs[4][i] = 0
+					block.outputs[5][i] = 0
+			break;
+			case 1:
+			case 2:
+					block.outputs[0][i] = 0
+					block.outputs[1][i] = 10
+					block.outputs[2][i] = 0
+					block.outputs[3][i] = 0
+					block.outputs[4][i] = 0
+					block.outputs[5][i] = 0
+			break;
+			case 3:
+			case 4:
+					block.outputs[0][i] = 0
+					block.outputs[1][i] = 0
+					block.outputs[2][i] = 10
+					block.outputs[3][i] = 0
+					block.outputs[4][i] = 0
+					block.outputs[5][i] = 0
+			break;
+			case 5:
+			case 6:
+					block.outputs[0][i] = 0
+					block.outputs[1][i] = 0
+					block.outputs[2][i] = 0
+					block.outputs[3][i] = 10
+					block.outputs[4][i] = 0
+					block.outputs[5][i] = 0
+			break;
+			case 7:
+			case 8:
+					block.outputs[0][i] = 0
+					block.outputs[1][i] = 0
+					block.outputs[2][i] = 0
+					block.outputs[3][i] = 0
+					block.outputs[4][i] = 10
+					block.outputs[5][i] = 0
+			break;
+			case 9:
+					block.outputs[0][i] = 0
+					block.outputs[1][i] = 0
+					block.outputs[2][i] = 0
+					block.outputs[3][i] = 0
+					block.outputs[4][i] = 0
+					block.outputs[5][i] = 10
+			break;				
+
+
+		}
+
+		// }
+		// if (block.inputs[0][i] > 5){
+		// 	// if the switcher case is false, pass input 0 to output 0
+		// 	if (switcher === false) {
+		// 		block.outputs[0][i] = 10
+		// 		block.outputs[1][i] = 0
+				
+		// 	} // if the switcher case is true, pass input 0 to output 1
+		// 		else {
+		// 		block.outputs[1][i] = 10
+		// 		block.outputs[0][i] = 0
+		// 	}	
+		// }
+
 	} 
+	
+
 }
